cb9aee70b217f7c11237c43ca337fa30ddb1e381
pavankumar730 <pp3421117@gmail.com>
2025-06-25 15:17:07 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,21 @@
+document.title="Dom in javaScript";
+
+document.getElementById("header").innerHTML="Hello World!";
+
+document.body.style.backgroundColor="lightblue";
+
+document.getElementById("header").style.color="red";
+
+document.getElementById("h").style.color="red"
+
+document.getElementsByClassName("ds")[0].style.color="blue";
+document.getElementsByClassName("ds")[1].style.color="grey";
+document.getElementsByClassName("ds")[2].style.color="orange";
+document.getElementsByClassName("ds")[3].style.color="green";
+document.getElementsByClassName("ds")[4].style.color="yellow";
+
+
+document.getElementsByTagName("h1")[0].style.color="red";
+
+
+
